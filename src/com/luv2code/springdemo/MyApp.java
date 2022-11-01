bf5bad3659d062105025633c7fef763bcfcec993
MJ <linuxdev@localhost.localdomain>
2022-11-01 14:18:56 +0530

    Spring Dependency Injection using XML Configuration

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+	//	Coach theCoach = new BaseballCoach();
+		
+	//	System.out.println(theCoach.getDailyWorkout());
+		
+		
+	//	Coach thecoach2 = new TrackCoach();
+	//	System.out.println(thecoach2.getDailyWorkout());
+
+	}
+
+}
